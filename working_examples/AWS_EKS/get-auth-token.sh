099201f880f3381f98d5a21a4feec03371199df2
JGeddes <botagar@gmail.com>
2020-01-30 16:43:01 +1000

    Adding instructions on connected to kube dashboard in auth script

--- working_examples/AWS_EKS/get-auth-token.sh
+++ working_examples/AWS_EKS/get-auth-token.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
+
+echo ""
+echo "To connect to the kubernetes cluster, run the proxy server. The proxy server can be started with 'kubectl proxy'"
+echo "Once the proxy has been started, go to: http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login"
+echo "Use the token above to sign in as admin"
+echo ""
